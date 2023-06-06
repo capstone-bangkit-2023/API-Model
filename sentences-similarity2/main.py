85d850ed21f1d79dd6599778c7b1cf88814b44a8
Niar Salsa <72489239+justniar@users.noreply.github.com>
2023-06-06 14:50:44 +0700

    Add files via upload

---
+++ sentences-similarity2/main.py
@@ -0,0 +1,23 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+from model import model_predict
+
+app = FastAPI()
+
+class TextRequest(BaseModel):
+    data: str
+    answer: str
+
+@app.get('/')
+def hello():
+    return ('Hello World!')
+
+@app.post("/predict")
+async def predict(text_request: TextRequest):
+    data, answer = text_request.data, text_request.answer
+    result = model_predict(data, answer)
+    return {'data': result}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
