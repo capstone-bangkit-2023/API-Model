f8976a4739e2b6d9a46fbe811d406db18f0cb3dd
Niar Salsa <72489239+justniar@users.noreply.github.com>
2023-06-16 19:06:21 +0700

    Update model.py

--- sentences-similarity/model.py
+++ sentences-similarity/model.py
@@ -2,19 +2,23 @@ import tensorflow as tf
 from sentence_transformers import SentenceTransformer, util
 from sklearn.metrics.pairwise import cosine_similarity
 
+# Memuat pre-trained Universal Sentence Encoder
+# embedder = SentenceTransformer('all-mpnet-base-v2')
 embedder = SentenceTransformer('paraphrase-albert-small-v2')
 
 def model_predict(data, answer):
-    sentences1 = [data]
-    sentences2 = [answer]
+    sentences1, sentences2 = [data], [answer]
     embeddings1 = embedder.encode(sentences1, convert_to_tensor=True)
     embeddings2 = embedder.encode(sentences2, convert_to_tensor=True)
+    # Menghitung kemiripan antara embedding kalimat menggunakan cosine similarity
     cosine_scores = util.pytorch_cos_sim(embeddings1, embeddings2)
     similarity_scores = tf.reduce_sum(tf.multiply(tf.nn.l2_normalize(embeddings1, axis=1),
                                                 tf.nn.l2_normalize(embeddings2, axis=1)),
                                                 axis=1)
+
     similarity = cosine_similarity(embeddings1.numpy(), embeddings2.numpy())
     result = [cosine_scores[0][0].item(), similarity_scores[0].numpy(), similarity[0][0]]
-    result = max(result)
-    result = int(result * 100)
-    return result
+    return max(result)
+
+#result = model_predict(embedder, data, answer)
+#print(result)
